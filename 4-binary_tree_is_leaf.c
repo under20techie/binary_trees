8d5b069482273dd18b4b29f0908c05bf64026dc3
Your Name <you@example.com>
2024-03-01 00:52:17 +0100

    Delete binary tree func

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - checks if node is a leaf
+ * @node: node to check
+ *
+ * Return: 1 if true else 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->left && !node->right)
+		return (1);
+	return (0);
+}
