fa06d4c64ef5dd9357f82a3336d894155ad39984
Your Name <you@example.com>
2024-03-01 01:40:07 +0100

    Delete binary tree func

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * * binary_tree_height - returns height of a tree
+ * * @tree: tree to determine height
+ * *
+ * * Return: height of tree
+ * */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (1 + max(left, right));
+}
